faca0e5140cfc39236ae0ea676141d3df28f13d2
Kni9t <clone111@bk.ru>
2022-11-02 19:57:00 +0300

    BasicAlgorithm
    Heavily not optimized

--- WPFProjectGameOfLife/MainWindow.xaml.cs
+++ WPFProjectGameOfLife/MainWindow.xaml.cs
@@ -12,17 +12,158 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace WPFProjectGameOfLife
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        public const int Edge = 10; // Длина ребра куба клетки
+        int MapHeight, MapWidth;
+        bool[][] Map; // Height на Width
+        int StringCheck(int id)
+        {
+            if (id >= MapHeight) return 0;
+            if (id < 0) return (MapHeight - 1);
+            return id;
+        }
+        int ColumCheck(int id)
+        {
+            if (id >= MapWidth) return 0;
+            if (id < 0) return (MapWidth - 1);
+            return id;
+        }
+        void UpdateLogic()
+        {
+            bool[][] Buf = new bool[MapHeight][];
+
+            for (int i = 0; i < MapHeight; i++)
+            {
+                Buf[i] = new bool[MapWidth];
+
+                for (int j = 0; j < MapWidth; j++)
+                {
+                    int bufcount = 0;
+
+                    if (Map[StringCheck(i - 1)][ColumCheck(j - 1)]) bufcount++;
+                    if (Map[StringCheck(i - 1)][ColumCheck(j)]) bufcount++;
+                    if (Map[StringCheck(i - 1)][ColumCheck(j + 1)]) bufcount++;
+
+                    if (Map[StringCheck(i)][ColumCheck(j - 1)]) bufcount++;
+                    if (Map[StringCheck(i)][ColumCheck(j + 1)]) bufcount++;
+
+                    if (Map[StringCheck(i + 1)][ColumCheck(j - 1)]) bufcount++;
+                    if (Map[StringCheck(i + 1)][ColumCheck(j)]) bufcount++;
+                    if (Map[StringCheck(i + 1)][ColumCheck(j + 1)]) bufcount++;
+
+                    if (Map[i][j] == false) // Стандарт
+                    {
+                        if (bufcount == 3) Buf[i][j] = true;
+                        else Buf[i][j] = false;
+                    }
+                    else
+                    {
+                        if ((bufcount == 2) || (bufcount == 3)) Buf[i][j] = true;
+                        else Buf[i][j] = false;
+                    }
+                }
+            }
+
+            Map = Buf;
+        }
+        void UpdatePrint()
+        {
+            Polygon bufPolygon;
+            PointCollection bufPointCollection;
+
+            MainScreen.Children.Clear();
+
+            for (int i = 0; i < MapHeight; i++)
+                for (int j = 0; j < MapWidth; j++)
+                {
+                    bufPolygon = new Polygon();
+                    bufPointCollection = new PointCollection();
+
+                    if (Map[i][j])
+                    {
+                        bufPointCollection.Add(new Point(Edge * j, Edge * i));
+                        bufPointCollection.Add(new Point(Edge * j, Edge * i + Edge));
+                        bufPointCollection.Add(new Point(Edge * j + Edge, Edge * i + Edge));
+                        bufPointCollection.Add(new Point(Edge * j + Edge, Edge * i));
+                        bufPolygon.Points = bufPointCollection;
+                        bufPolygon.Fill = Brushes.White;
+                    }
+                    else
+                    {
+                        bufPointCollection.Add(new Point(Edge * j, Edge * i));
+                        bufPointCollection.Add(new Point(Edge * j, Edge * i + Edge));
+                        bufPointCollection.Add(new Point(Edge * j + Edge, Edge * i + Edge));
+                        bufPointCollection.Add(new Point(Edge * j + Edge, Edge * i));
+                        bufPolygon.Points = bufPointCollection;
+                        bufPolygon.Fill = Brushes.Black;
+                        bufPolygon.Stroke = Brushes.Black;
+                    }
+                    MainScreen.Children.Add(bufPolygon);
+                }
+            GC.Collect();
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            MapHeight = (int)(Window.Height / Edge);
+            MapWidth = (int)(Window.Width / Edge);
+
+            Map = new bool[MapHeight][];
+
+            for (int i = 0; i < MapHeight; i++)
+            {
+                Map[i] = new bool[MapWidth];
+
+                for (int j = 0; j < MapWidth; j++)
+                {
+                    Map[i][j] = false;
+                }
+            }
+            DispatcherTimer Timer = new DispatcherTimer();
+            Timer.Tick += new EventHandler(Timer_Tick);
+            Timer.Interval = new TimeSpan(0, 0, 0, 0, 500);
+            Timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            UpdateLogic();
+            UpdatePrint();
+        }
+
+        private void MouseClick(object sender, MouseEventArgs e)
+        {
+            Random R = new Random();
+
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                int PositionX = (int)e.GetPosition(this).X / Edge, PositionY = (int)e.GetPosition(this).Y / Edge;
+
+                if (R.Next(0, 2) == 1) Map[StringCheck(PositionY - 1)][ColumCheck(PositionX - 1)] = true;
+                if (R.Next(0, 2) == 1) Map[StringCheck(PositionY - 1)][ColumCheck(PositionX)] = true;
+                if (R.Next(0, 2) == 1) Map[StringCheck(PositionY - 1)][ColumCheck(PositionX + 1)] = true;
+
+                if (R.Next(0, 2) == 1) Map[StringCheck(PositionY)][ColumCheck(PositionX - 1)] = true;
+                if (R.Next(0, 2) == 1) Map[StringCheck(PositionY)][ColumCheck(PositionX)] = true;
+                if (R.Next(0, 2) == 1) Map[StringCheck(PositionY)][ColumCheck(PositionX + 1)] = true;
+
+                if (R.Next(0, 2) == 1) Map[StringCheck(PositionY + 1)][ColumCheck(PositionX - 1)] = true;
+                if (R.Next(0, 2) == 1) Map[StringCheck(PositionY + 1)][ColumCheck(PositionX)] = true;
+                if (R.Next(0, 2) == 1) Map[StringCheck(PositionY + 1)][ColumCheck(PositionX + 1)] = true;
+            }
+
+            if (e.RightButton == MouseButtonState.Pressed)
+            {
+                int PositionX = (int)e.GetPosition(this).X / Edge, PositionY = (int)e.GetPosition(this).Y / Edge;
+
+                if (R.Next(0, 2) == 1) Map[StringCheck(PositionY)][ColumCheck(PositionX)] = true;
+            }
         }
     }
 }
